b25cb2fab4aabfc99b85e255160be40c0ebe20eb
Israel <isril.reyes@gmail.com>
2026-06-11 21:10:45 -0600

    correccion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { BirdsModule } from './birds/birds.module';
 
 @Module({
   imports: [CatsModule, BirdsModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
